5a10497a4e9e46a1c9118658df1282490cbb2feb
zhixx0830 <a0966086485@gmail.com>
2026-05-12 11:30:31 +0800

    webhook2

--- web.py
+++ web.py
@@ -371,9 +371,13 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "我是許芷嫙的電影聊天機器人,動作：" + action + "； 查詢內容：" + msg
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "我是許芷嫙設計的電影聊天機器人,動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是許芷嫙設計的電影聊天機器人,您選擇的電影分級是：" + rate
     return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run()
